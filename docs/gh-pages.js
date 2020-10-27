eeb6bc55a3c022d4ad30fb319af24130e8b4a2f6
Godfery <hiyangguo@qq.com>
2020-10-27 14:20:14 +0800

    Added publish docs action

--- docs/gh-pages.js
+++ docs/gh-pages.js
@@ -2,5 +2,9 @@ var ghpages = require('gh-pages');
 var path = require('path');
 
 ghpages.publish(path.join(__dirname, '../assets'), function(err) {
-  console.log(err);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Publish successful');
 });
