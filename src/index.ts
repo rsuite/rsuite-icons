7c437416b2d7f0aaf33191ef4509fe096a29a105
Godfery <hiyangguo@qq.com>
2020-11-26 15:32:10 +0800

    Change epxort

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { default as IconBase, IconBaseProps, IconProps } from './IconBase';
+export * from './IconBase';
+export { default as IconBase } from './IconBase';
 export { default as Icon } from './Icon';
 export { default as createIconFont } from './IconFont';
 export * from './icons';
