1a0f7743112c3e0f24d80f5999d5c2a5dcfcd4c7
simonguo <simonguo.2009@gmail.com>
2024-10-16 10:48:48 +0800

    ci: refactor build code

--- scripts/proxyDirectories.js
+++ scripts/proxyDirectories.js
@@ -1,9 +1,3 @@
-/**
- * Create a package.json for each directory and proxy to CJS and ESM files.
- * Can make importing a component easier.
- *
- */
-
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
@@ -13,91 +7,132 @@ const writeFile = util.promisify(fs.writeFile);
 const srcRoot = path.join(__dirname, '../src');
 const libRoot = path.join(__dirname, '../lib');
 
-function findResources(options) {
-  const { dir = srcRoot, ignores = [], isFile } = options;
-  const resources = [];
-  fs.readdirSync(dir).forEach(item => {
+/**
+ * Recursively finds resources in a directory, with optional filters for files and ignored directories.
+ * @param {Object} options - The options for finding resources.
+ * @param {string} options.dir - The directory to search in.
+ * @param {string[]} options.ignores - List of directories to ignore.
+ * @param {boolean} options.isFile - Whether to include files in the search results.
+ * @returns {string[]} - List of found resources.
+ */
+function findResources({ dir = srcRoot, ignores = [], isFile }) {
+  const resources = fs.readdirSync(dir).map(item => {
     const itemPath = path.resolve(dir, item);
     const pathname = itemPath.replace(/[a-z0-9\-]*\//gi, '').replace('.tsx', '');
 
-    if (fs.statSync(itemPath).isDirectory()) {
-      resources.push(pathname);
+    const isDirectory = fs.statSync(itemPath).isDirectory();
+    const isFileMatch = isFile && fs.statSync(itemPath).isFile();
+
+    if (isDirectory || isFileMatch) {
+      return pathname;
     }
-    if (isFile && fs.statSync(itemPath).isFile()) {
-      resources.push(pathname);
-    }
+    return null;
   });
 
-  //console.log(resources);
-
-  return resources.filter(item => !ignores.includes(item));
+  return resources.filter(item => item && !ignores.includes(item));
 }
 
-function proxyResource(options) {
-  const { pkgName = '@rsuite/icons', name, file, filePath = '../', subPath = '/' } = options;
-  const proxyPkg = {
-    name: `${pkgName}/${name}`,
-    private: true,
-    main: `${filePath}/cjs${subPath}${file}.js`,
-    module: `${filePath}/esm${subPath}${file}.js`,
-    types: `${filePath}/esm${subPath}${file}.d.ts`
-  };
-
-  return JSON.stringify(proxyPkg, null, 2) + '\n';
+/**
+ * Creates a proxy package.json content.
+ * @param {Object} options - The options for the proxy.
+ * @param {string} options.pkgName - The base package name.
+ * @param {string} options.name - The name of the resource.
+ * @param {string} options.file - The file name without extension.
+ * @param {string} [options.filePath='../'] - The relative path to the resource.
+ * @param {string} [options.subPath='/'] - The subpath inside the package.
+ * @returns {string} - The package.json content.
+ */
+function proxyResource({ pkgName = '@rsuite/icons', name, file, filePath = '../', subPath = '/' }) {
+  return (
+    JSON.stringify(
+      {
+        name: `${pkgName}/${name}`,
+        private: true,
+        main: `${filePath}/cjs${subPath}${file}.js`,
+        module: `${filePath}/esm${subPath}${file}.js`,
+        types: `${filePath}/esm${subPath}${file}.d.ts`
+      },
+      null,
+      2
+    ) + '\n'
+  );
 }
 
-async function writePkgFile(options) {
-  const {
-    resources = [],
-    pkgName = '@rsuite/icons',
-    dir = libRoot,
-    subPath,
-    filePath = '..'
-  } = options;
+/**
+ * Writes a package.json file for each resource in the specified directory.
+ * @param {Object} options - The options for writing the package file.
+ * @param {string[]} options.resources - The list of resources.
+ * @param {string} options.pkgName - The base package name.
+ * @param {string} options.dir - The directory where package.json will be written.
+ * @param {string} options.subPath - The subpath for the resource.
+ * @param {string} options.filePath - The file path to the resource.
+ */
+async function writePkgFile({
+  resources = [],
+  pkgName = '@rsuite/icons',
+  dir = libRoot,
+  subPath,
+  filePath = '..'
+}) {
   await Promise.all(
     resources.map(async item => {
-      const name = item;
-      const file = `${item}`;
-      const proxyDir = path.join(dir, name);
-      await mkDir(dir).catch(() => {});
-      await mkDir(proxyDir).catch(() => {});
-      await writeFile(
-        `${proxyDir}/package.json`,
-        proxyResource({ pkgName, name, file, filePath, subPath })
-      ).catch(err => {
-        if (err) console.error(err.toString());
-      });
+      try {
+        const name = item;
+        const file = item;
+        const proxyDir = path.join(dir, name);
+
+        await mkDir(proxyDir, { recursive: true }); // Ensure directory creation is recursive
+        await writeFile(
+          `${proxyDir}/package.json`,
+          proxyResource({ pkgName, name, file, filePath, subPath })
+        );
+      } catch (error) {
+        console.error(`Error writing package.json for ${item}:`, error);
+      }
     })
   );
 }
 
+/**
+ * Main function to proxy components.
+ */
 async function proxyComponent() {
-  const icons = findResources({
-    dir: path.join(srcRoot, 'icons'),
-    isFile: true,
-    ignores: ['legacy']
-  });
+  try {
+    const icons = findResources({
+      dir: path.join(srcRoot, 'icons'),
+      isFile: true,
+      ignores: ['legacy']
+    });
 
-  await writePkgFile({ resources: icons, subPath: '/icons/' });
+    await writePkgFile({ resources: icons, subPath: '/icons/' });
 
-  const legacyIcons = findResources({ dir: path.join(srcRoot, 'icons/legacy'), isFile: true });
+    const legacyIcons = findResources({
+      dir: path.join(srcRoot, 'icons/legacy'),
+      isFile: true
+    });
 
-  await writePkgFile({
-    resources: legacyIcons,
-    pkgName: '@rsuite/icons/legacy',
-    subPath: '/icons/legacy/',
-    filePath: '../..',
-    dir: path.join(libRoot, 'legacy')
-  });
+    await writePkgFile({
+      resources: legacyIcons,
+      pkgName: '@rsuite/icons/legacy',
+      subPath: '/icons/legacy/',
+      filePath: '../..',
+      dir: path.join(libRoot, 'legacy')
+    });
 
-  const components = ['Icon', 'IconProvider', 'createIconFont', 'createSvgIcon'];
-
-  await writePkgFile({ resources: components });
+    const components = ['Icon', 'IconProvider', 'createIconFont', 'createSvgIcon'];
+    await writePkgFile({ resources: components });
+  } catch (error) {
+    console.error('Error in proxyComponent:', error);
+  }
 }
 
-async function proxy() {
-  await proxyComponent();
-}
-
-module.exports.findResources = findResources;
-module.exports.default = proxy;
+/**
+ * Entry point function to trigger the proxying process.
+ */
+module.exports = async function proxy() {
+  try {
+    await proxyComponent();
+  } catch (error) {
+    console.error('Error in proxy:', error);
+  }
+};
